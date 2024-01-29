01e09eb9bf2b2a3ed7c34a49601d0da406d07629
taku10101 <taku1011ht@gmail.com>
2024-01-30 01:47:57 +0900

    key change add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
@@ -63,6 +63,19 @@ export const Home = () => {
                 setErrorMessage(`タスクの取得に失敗しました。${err}`);
             });
     };
+    const keyMove = (e) => {
+        if (e.key === "ArrowUp") {
+            const moveIndex = lists.findIndex((list) => list.id === selectListId); // 選択中のリストのインデックスを取得
+            const previousIndex = (moveIndex - 1 + lists.length) % lists.length; // 選択中のリストの一つ前のインデックスを取得 %でリストの数を超えないようにする
+            const previousId = lists[previousIndex].id; // 選択中のリストの一つ前のリストのIDを取得
+            handleSelectList(previousId);
+        } else if (e.key === "ArrowDown") {
+            const moveIndex = lists.findIndex((list) => list.id === selectListId);
+            const nextIndex = (moveIndex + 1) % lists.length;
+            const nextId = lists[nextIndex].id;
+            handleSelectList(nextId);
+        }
+    };
     return (
         <div>
             <Header />
@@ -85,16 +98,12 @@ export const Home = () => {
                             const isActive = list.id === selectListId;
                             return (
                                 <li
+                                    onKeyDown={keyMove}
+                                    tabIndex={0}
                                     key={key}
                                     className={`list-tab-item ${isActive ? "active" : ""}`}
                                     onClick={() => handleSelectList(list.id)}
                                 >
-                                    <input
-                                        type="radio"
-                                        name="list-tab-item"
-                                        onChange={() => handleSelectList(list.id)}
-                                        checked={selectListId === list.id}
-                                    />
                                     {list.title}
                                 </li>
                             );
@@ -121,73 +130,44 @@ export const Home = () => {
 
 // 表示するタスク
 const Tasks = (props) => {
-    // eslint-disable-next-line
-    const tasks = props.tasks;
-    // eslint-disable-next-line
-    const selectListId = props.selectListId;
-    // eslint-disable-next-line
-    const isDoneDisplay = props.isDoneDisplay;
-    const isDone = isDoneDisplay === "done";
+    const { tasks, selectListId, isDoneDisplay } = props;
     if (tasks === null) return <></>;
+
+    if (isDoneDisplay === "done") {
+        return (
+            <ul>
+                {tasks
+                    .filter((task) => {
+                        return task.done === true;
+                    })
+                    .map((task, key) => (
+                        <li key={key} className="task-item" role="button">
+                            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
+                                {task.title}
+                                <br />
+                                {task.done ? "完了" : "未完了"}
+                            </Link>
+                        </li>
+                    ))}
+            </ul>
+        );
+    }
 
     return (
         <ul>
             {tasks
-                // eslint-disable-next-line
                 .filter((task) => {
-                    return task.done === isDone;
+                    return task.done === false;
                 })
-                .map((task, key) => {
-                    const date = new Date(task.limit);
-                    const pad = function (str) {
-                        return ("0" + str).slice(-2);
-                    };
-                    // 表示用
-                    const year = date.getFullYear().toString();
-                    const month = pad((date.getMonth() + 1).toString());
-                    const day = pad(date.getDate().toString());
-                    const hour = pad(date.getHours().toString());
-                    const min = pad(date.getMinutes().toString());
-                    const text = `${year}年${month}月${day}日 ${hour}:${min}`;
-
-                    // 現在時刻を取得
-                    const currentDate = new Date();
-
-                    // 差分を取得
-
-                    const timeDiff = Math.ceil((date - currentDate) / (1000 * 60));
-                    const resDiff = timeDiff < 0 ? -timeDiff : timeDiff;
-
-                    // 残りの日数、時間、分を計算
-                    const daysDiff = Math.floor(resDiff / (60 * 24));
-                    const hoursDiff = Math.floor((resDiff % (60 * 24)) / 60);
-                    const minDiff = Math.floor(resDiff % 60);
-
-                    // 残り時間のテキストを格納
-                    const textLimit =
-                        timeDiff >= 0
-                            ? `残り${daysDiff}日${hoursDiff}時間${minDiff}分`
-                            : `${Math.abs(daysDiff)}日${Math.abs(hoursDiff)}時間${Math.abs(minDiff)}分 遅れ`;
-
-                    return (
-                        <li key={key} className="task-item">
-                            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
-                                <div className="task-item-link-left">
-                                    <span>{task.title}</span>
-                                </div>
-                                <span className="vertical">{task.done ? "完了" : "未完了"}</span>
-                                <div className="task-item-link-right">
-                                    <span>{text}</span>
-                                    {task.done ? (
-                                        ""
-                                    ) : (
-                                        <span style={{ color: timeDiff >= 0 ? "initial" : "red" }}>{textLimit}</span>
-                                    )}
-                                </div>
-                            </Link>
-                        </li>
-                    );
-                })}
+                .map((task, key) => (
+                    <li key={key} className="task-item">
+                        <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
+                            {task.title}
+                            <br />
+                            {task.done ? "完了" : "未完了"}
+                        </Link>
+                    </li>
+                ))}
         </ul>
     );
 };
